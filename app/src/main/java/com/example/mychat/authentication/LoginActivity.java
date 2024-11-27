9c6929fa4d3e845d2a5650cefc1535958314725d
Lior S. <79309515+DeveloperCron@users.noreply.github.com>
2024-11-27 17:57:01 +0200

    Initialized project

---
+++ app/src/main/java/com/example/mychat/authentication/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.mychat.authentication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mychat.R;
+import com.example.mychat.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
